3c89acd6ae7caad78ce9a70672ed46255ba7242a
JBKIM <drv98@naver.com>
2024-06-13 10:14:31 +0900

    ImageCard 표시

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import ImageCard from "./components/ImageCard";
 
 function App() {
   const [images, setImages] = useState([]); //이미지배열
@@ -16,38 +17,11 @@ function App() {
   }, []);
 
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img
-        src="https://images.unsplash.com/photo-1495360010541-f48722b34f7d?q=80&w=1936&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        alt=""
-        className="w-full"
-      />
-      <div className="px-6 py-4">
-        <div className="font-bold text-purple-500 text-xl mb-2">
-          Photo by John Doe
-        </div>
-        <ul>
-          <li>
-            <strong>Views: </strong> 4000
-          </li>
-          <li>
-            <strong>Downloads: </strong> 300
-          </li>
-          <li>
-            <strong>Likes: </strong> 400
-          </li>
-        </ul>
-      </div>
-      <div className="px-6 py-4">
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #tag1
-        </span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #tag2
-        </span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #tag3
-        </span>
+    <div className="container mx-auto my-7">
+      <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7">
+        {images.map((img) => (
+          <ImageCard key={img.id} image={img} />
+        ))}
       </div>
     </div>
   );
