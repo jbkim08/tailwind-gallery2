5f7dfa8abc7e1de663db4db1aae147aef77016e6
JBKIM <drv98@naver.com>
2024-06-13 09:40:34 +0900

    픽사베이 API 요청

--- src/App.js
+++ src/App.js
@@ -1,4 +1,20 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [images, setImages] = useState([]); //이미지배열
+  const [isLoading, setIsLoding] = useState(true); //로딩중 상태
+  const [term, setTerm] = useState("flowers"); //검색어
+  //처음 시작시 이미지들을 받아서 images에 저장
+  useEffect(() => {
+    //fecth메소드로 픽사베에서 검색한 이미지 데이터들을 받음
+    fetch(
+      `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo`
+    )
+      .then((res) => res.json())
+      .then((data) => setImages(data.hits))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg">
       <img
